19e4b734ed64764964240faab9e524e1ca9d7daa
Amit-Prajapati <amitprajapati.cmpn@gmail.com>
2017-12-27 11:14:41 -0600

    Create firstExercise.py

---
+++ Chapter 2/firstExercise.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+
+first_name = 'Amit'
+last_name = 'Prajapati'
+age = 27
+birth_date = '30-Aug-1991'
+
+print("My name is %s %s,\nI was born on %s and I am %s years old" % (first_name, last_name, birth_date, age))
+
+>>Output:--
+bin $ ./firstExercise.py
+My name is Amit Prajapati,
+I was born on 30-Aug-1991 and I am 27 years old
