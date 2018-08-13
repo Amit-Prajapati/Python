e0f4a1110b3d9e396da0abdfe665ebb89abbffb1
Amit-Prajapati <amitprajapati.cmpn@gmail.com>
2018-08-13 17:37:23 +0530

    Update extraExcercise.py

--- Chapter 2/extraExcercise.py
+++ Chapter 2/extraExcercise.py
@@ -22,3 +22,20 @@ orange
 pear
 pear
 ------------------------------------------------------------
+Unpacking variadic positional arguments
+
+>>> def is_prime(n):
+...     if n > 1:
+...         for i in range(2, n):
+...             if (n % i) == 0:
+...                 return
+...             else:
+...                 return n
+>>> def product(*n, a=1): // n can be single or list of data
+...     for i in n:
+...         a *= i
+...     return a
+...
+>>> primes = [number for number in range(2,100) if is_prime(number)]
+>>> print(product(*primes)) //The syntax *seq unpacks a sequence into its constituent components
+2725392139750729502980713245400918633290796330545803413734328823443106201171875
