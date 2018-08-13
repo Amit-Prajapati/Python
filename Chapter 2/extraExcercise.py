cefffd9a79335535710541a235899b122819ad09
Amit-Prajapati <amitprajapati.cmpn@gmail.com>
2018-08-13 16:22:24 +0530

    Update extraExcercise.py

--- Chapter 2/extraExcercise.py
+++ Chapter 2/extraExcercise.py
@@ -1,3 +1,4 @@
+Reversed Iteration:
 Example 1: To loop over a sequence in reverse, first specify the sequence in forward direction "range(start, end, with step of)" and
             then call reversed() function.
 >>> for i in reversed(range(0, 10, 2)):
@@ -8,4 +9,16 @@ Example 1: To loop over a sequence in reverse, first specify the sequence in for
 4
 2
 0
-
+------------------------------------------------------------
+Sorted Iteration:
+Example: To loop over a sequence in sorted order, use the sorted() function which returns a new sorted list while leaving the sort unaltered.
+>>> basket = ['pear', 'banana', 'orange', 'pear', 'apple']
+>>> for fruit in sorted(basket):
+...     print(fruit)
+...
+apple
+banana
+orange
+pear
+pear
+------------------------------------------------------------
